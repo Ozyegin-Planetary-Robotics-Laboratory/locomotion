2463bf9bf5285003f12380024d91e79b046cc4d3
Toprak-Efe <efe.akkilic@ozu.edu.tr>
2024-02-29 00:07:12 +0300

    8

--- scripts/main.py
+++ scripts/main.py
@@ -8,16 +8,39 @@ import csv
 import rospy
 import time
 
+wheel_speeds = [.0, .0, .0, .0]
+
+def joy_callback(data):
+  global wheel_speeds
+  wheel_speeds[0] = data.axes[1]
+  wheel_speeds[1] = data.axes[1]
+  wheel_speeds[2] = data.axes[4]
+  wheel_speeds[3] = data.axes[4]  
 
 if __name__=="__main__":
   rospy.init_node("ozurover_locomotion", anonymous=True)
   with TMotorManager_servo_can(motor_type="AK70-10", motor_ID=1) as motor1:
-    loop = SoftRealtimeLoop(dt=0.0001, report=True, fade=0.0)
-    motor1.enter_velocity_control()
-    time.sleep(1)
-    
-    for t in loop:
-      motor1.set_velocity(0.1)
-      motor1.update()
-      if loop.kill_now:
-        break
+    with TMotorManager_servo_can(motor_type="AK70-10", motor_ID=2) as motor2:
+      with TMotorManager_servo_can(motor_type="AK70-10", motor_ID=3) as motor3:
+        with TMotorManager_servo_can(motor_type="AK70-10", motor_ID=4) as motor4:
+          
+          def update_loop():
+            loop = SoftRealtimeLoop(dt=0.0001, report=False, fade=0.0)
+            for t in loop:
+              motor1.velocity = wheel_speeds[0]
+              motor2.velocity = wheel_speeds[1]
+              motor3.velocity = wheel_speeds[2]
+              motor4.velocity = wheel_speeds[3]
+              motor1.update()
+          
+          motor1.enter_velocity_control()
+          motor2.enter_velocity_control()
+          motor3.enter_velocity_control()
+          motor4.enter_velocity_control()
+          time.sleep(1)
+          
+          update_thread = threading.Thread(target=update_loop)
+          update_thread.start()
+          
+          rospy.Subscriber("joy", Joy, joy_callback)
+          rospy.spin()
