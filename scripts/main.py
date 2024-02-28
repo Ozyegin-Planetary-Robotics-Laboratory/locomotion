fd515c408e064b9ea0a25a7df5a21452fad23cda
Toprak-Efe <efe.akkilic@ozu.edu.tr>
2024-02-28 23:27:20 +0300

    6

--- scripts/main.py
+++ scripts/main.py
@@ -1,66 +1,46 @@
 #!/usr/bin/python3
 from sensor_msgs.msg import Joy
+from NeuroLocoMiddleware.SoftRealtimeLoop import SoftRealtimeLoop
 from TMotorCANControl.servo_can import TMotorManager_servo_can
+from datetime import datetime, timezone, timedelta
 import threading
+import csv
 import rospy
 import time
 
-motors = []
-speed = 0.0
-alive = True
+start_time = time.time()
+
+class MotorController:
+    def __init__(self):
+        time.sleep(2.0)
+        rospy.init_node("ozurover_motor_interface", anonymous=True)
+        self.subscriber = rospy.Subscriber("/joy", Joy, self.joy_callback)
+
+    def getTime(self):
+        current_time = time.time() - start_time
+        return current_time
+    
+    def joy_callback(self, joystick_data):
+        l_joy_val, r_joy_val = float(joystick_data.axes[1]), float(joystick_data.axes[4])
+        angular_vel_range, joy_range = 13.6, 2.0
+        speed_coefficient = 0.5 #Coefficient to tame the speed output.
+        motor1.set_motor_velocity_radians_per_second((-r_joy_val * angular_vel_range/joy_range) * speed_coefficient)
+        motor2.set_motor_velocity_radians_per_second((l_joy_val * angular_vel_range/joy_range) * speed_coefficient)
+        motor3.set_motor_velocity_radians_per_second((-r_joy_val * angular_vel_range/joy_range) * speed_coefficient)
+        motor4.set_motor_velocity_radians_per_second((l_joy_val * angular_vel_range/joy_range) * speed_coefficient) 
+        #motor1.velocity = (-r_joy_val * angular_vel_range/joy_range) * speed_coefficient
+        #motor2.velocity = (l_joy_val * angular_vel_range/joy_range) * speed_coefficient
+        #motor3.velocity = (-r_joy_val * angular_vel_range/joy_range) * speed_coefficient
+        #motor4.velocity = (l_joy_val * angular_vel_range/joy_range) * speed_coefficient
 
 if __name__ == '__main__':
     with TMotorManager_servo_can(motor_type='AK70-10', motor_ID=1) as motor1:
         with TMotorManager_servo_can(motor_type='AK70-10', motor_ID=2) as motor2:
             with TMotorManager_servo_can(motor_type='AK70-10', motor_ID=3) as motor3:
                 with TMotorManager_servo_can(motor_type='AK70-10', motor_ID=4) as motor4:
-
-                    motors.append(motor1)
-                    motors.append(motor2)
-                    motors.append(motor3)
-                    motors.append(motor4)
-                    
-                    motor1.enter_velocity_control()
-                    motor2.enter_velocity_control()
-                    motor3.enter_velocity_control()
-                    motor4.enter_velocity_control()
-                    
-                    def motorGoBr():
-                      global motors, speed, alive
-                      while alive:
-                        motors[1].velocity = speed
-                        motors[2].velocity = speed
-                        motors[3].velocity = speed
-                        motors[4].velocity = speed
-                        time.sleep(0.1)    
-                    thread = threading.Thread(target=motorGoBr).start()
-                    
-                    # Input loop
-                    # q: quit
-                    # w: forward
-                    # s: backward
-                    # a: abort
-                    c_in = 'b'
-                    while c_in != 'q':
-                        c_in = input("Enter command: ")
-                        if c_in == 'w':
-                          speed += 0.1
-                        elif c_in == 's':
-                          speed += -0.1
-                        elif c_in == 'a':
-                          speed = 0.0
-                        elif c_in == 'q':
-                          speed = 0.0
-                          break
-                        else:
-                          print("Invalid command")
-                          
-                    
-                    print("Exiting...")
-                    motor1.velocity = 0.0
-                    motor2.velocity = 0.0
-                    motor3.velocity = 0.0
-                    motor4.velocity = 0.0
-                    
-                    alive = False
-                    thread.join()
+                      motor1.enter_velocity_control()
+                      motor2.enter_velocity_control()
+                      motor3.enter_velocity_control()
+                      motor4.enter_velocity_control()
+                      motor_interface = MotorController()
+                      rospy.spin()
